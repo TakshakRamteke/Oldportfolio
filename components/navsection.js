0315638ef8ced3adf6475165e196defc6651df18
TakshakRamteke <takshakramteke0708@gmail.com>
2021-09-07 21:12:51 +0530

    enhancement : Added links in nav section.

--- components/navsection.js
+++ components/navsection.js
@@ -1,13 +1,30 @@
 import styles from './navsection.module.css'
+import Link from 'next/link'
 
 export default function NavSection({children}){
     return (
         <>
         <section className={styles.sec2}>
-            <div className={styles.row}></div>
-            <div className={styles.row}></div>
-            <div className={styles.row}></div>
-            <div className={styles.row}></div>
+            <div className={styles.row}>
+                <Link href="/">
+                    <a className={styles.linktext}><h1>About me →</h1></a>
+                </Link>
+            </div>
+            <div className={styles.row}>
+            <Link href="/">
+                    <a className={styles.linktext}><h1>Contact me →</h1></a>
+                </Link>
+            </div>
+            <div className={styles.row}>
+            <Link href="/">
+                    <a className={styles.linktext}><h1>Projects →</h1></a>
+                </Link>
+            </div>
+            <div className={styles.row}>
+            <Link href="/">
+                    <a className={styles.linktext}><h1> My art →</h1></a>
+                </Link>
+            </div>
         </section>
         </>
     )
