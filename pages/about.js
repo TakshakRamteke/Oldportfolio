bf434ccd1033fddeb59ce27c7463aaa0dad0b4d0
TakshakRamteke <takshakramteke0708@gmail.com>
2021-09-08 21:03:23 +0530

    enhancement : completed the about page including skills and about me section.

--- pages/about.js
+++ pages/about.js
@@ -1,7 +1,8 @@
 import Head from 'next/head'
+import Image from 'next/image'
 import Layout from '../components/layout'
 import styles from '../styles/typography.module.css'
-import utils from '../styles/utils.module.css'
+import abtstyl from '../styles/about.module.css'
 
 export default function About(){
     return(
@@ -27,6 +28,113 @@ export default function About(){
                             🎨 Also love making Sketche's sometimes.
                         </li>
                     </ul>
+                    <div>
+                        <h1 className={styles.headingSCenter}>Some of my skills</h1>
+                        <ul className={abtstyl.skillGrid}>
+                            <li className={abtstyl.skill}>
+                                Languages :
+                                <Image
+                                src="/icons/languages/python.png"
+                                width={37}
+                                height={37}
+                                /> 
+                                <Image 
+                                src="/icons/languages/JS.png"
+                                width={37}
+                                height={37}
+                                />
+                                <Image
+                                src="/icons/languages/bash.png"
+                                width={37}
+                                height={37}
+                                /> 
+                                <Image  
+                                src="/icons/languages/java.png"
+                                width={35}
+                                height={37}
+                                />
+                                <Image  
+                                src="/icons/languages/c++.png"
+                                width={32}
+                                height={37}
+                                />
+                            </li>
+                            <li className={abtstyl.skill}>
+                                Franeworks :
+                                <Image  
+                                src="/icons/frameworks/nextjs.png"
+                                width={32}
+                                height={32}
+                                />
+                                <Image  
+                                src="/icons/frameworks/node.png"
+                                width={33}
+                                height={37}
+                                />
+                                <Image  
+                                src="/icons/frameworks/react.png"
+                                width={43}
+                                height={37}
+                                />
+                                <Image  
+                                src="/icons/frameworks/django.png"
+                                width={27}
+                                height={37}
+                                />
+                                <Image  
+                                src="/icons/frameworks/flask.png"
+                                width={43}
+                                height={37}
+                                />
+                            </li>
+                            <li className={abtstyl.skill}>
+                                Tools : 
+                                <Image  
+                                src="/icons/tools/docker.png"
+                                width={46}
+                                height={37}
+                                />
+                                <Image  
+                                src="/icons/tools/inkscape.png"
+                                width={38}
+                                height={37}
+                                />
+                                <Image  
+                                src="/icons/tools/gimp.png"
+                                width={37}
+                                height={37}
+                                />
+                                <Image  
+                                src="/icons/tools/canva.png"
+                                width={37}
+                                height={37}
+                                />
+                            </li>
+                            <li className={abtstyl.skill}>
+                                Platforms :
+                                <Image  
+                                src="/icons/platforms/esp.png"
+                                width={37}
+                                height={37}
+                                />
+                                <Image  
+                                src="/icons/platforms/rpi.png"
+                                width={27}
+                                height={40}
+                                />
+                                <Image  
+                                src="/icons/platforms/arduino.png"
+                                width={37}
+                                height={37}
+                                />
+                                <Image  
+                                src="/icons/platforms/GC.png"
+                                width={48}
+                                height={37}
+                                />
+                            </li>
+                        </ul>
+                    </div>
             </Layout>
         </>    
     )
