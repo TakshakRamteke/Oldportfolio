786bde36514ac9edc369181d2615f3d940930bfb
TakshakRamteke <takshakramteke0708@gmail.com>
2021-09-08 18:30:26 +0530

    fix : altered some styling and made a few changes to about page.

--- pages/about.js
+++ pages/about.js
@@ -1,9 +1,19 @@
+import Head from 'next/head'
 import Layout from '../components/layout'
+import styles from '../styles/typography.module.css'
 
 export default function About(){
     return(
-        <Layout>
-            <h1>About me</h1>
-        </Layout>        
+        <>
+            <Head>
+                <title>About me</title>
+            </Head>
+            <Layout>
+                <h1 className={styles.headingL}>A bit 
+                </h1>
+                <h1 className={styles.dripyHeading}>About me
+                </h1>
+            </Layout>
+        </>    
     )
 }
