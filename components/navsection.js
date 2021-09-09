67427fd93eae3ec07431a0f51c00daf49e4da26b
TakshakRamteke <takshakramteke0708@gmail.com>
2021-09-09 12:52:58 +0530

    fix : fixed some styling on layout and about page.

--- components/navsection.js
+++ components/navsection.js
@@ -7,7 +7,7 @@ export default function NavSection({children}){
         <section className={styles.sec2}>
             <div className={styles.row}>
                 <Link href="/about">
-                    <a className={styles.linktext}><h1>About →</h1></a>
+                    <a className={styles.linktext}><h1> About →</h1></a>
                 </Link>
             </div>
             <div className={styles.row}>
