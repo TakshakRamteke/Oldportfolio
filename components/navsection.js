513d0b764554d6dc78093dd763c61d5981167e0c
TakshakRamteke <takshakramteke0708@gmail.com>
2021-09-10 12:23:11 +0530

    enhancement : added the functionality of showing currently active page.

--- components/navsection.js
+++ components/navsection.js
@@ -1,28 +1,47 @@
 import styles from './navsection.module.css'
 import Link from 'next/link'
+import {useRouter} from 'next/router'
 
 export default function NavSection({children}){
+    
+    const router = useRouter()
+    function isActive(route){
+        if(route==router.pathname){
+            return true
+        }else{
+            return false
+        }
+    }
+
     return (
         <>
         <section className={styles.sec2}>
             <div className={styles.row}>
                 <Link href="/about">
-                    <a className={styles.linktext}><h1> About →</h1></a>
+                    <a className={isActive('/about') ? styles.active : styles.linktext}>
+                        <h1> About →</h1>
+                    </a>
                 </Link>
             </div>
             <div className={styles.row}>
             <Link href="/">
-                    <a className={styles.linktext}><h1>Contact →</h1></a>
+                    <a className={isActive('/contact') ? styles.active : styles.linktext}>
+                        <h1>Contact →</h1>
+                    </a>
                 </Link>
             </div>
             <div className={styles.row}>
             <Link href="/">
-                    <a className={styles.linktext}><h1>Projects →</h1></a>
+                    <a className={isActive('/projects') ? styles.active : styles.linktext}>
+                        <h1>Projects →</h1>
+                    </a>
                 </Link>
             </div>
             <div className={styles.row}>
             <Link href="/">
-                    <a className={styles.linktext}><h1> My art →</h1></a>
+                    <a className={isActive('/myart') ? styles.active : styles.linktext}>
+                        <h1> My art →</h1>
+                    </a>
                 </Link>
             </div>
         </section>
